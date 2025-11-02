a6798b599259549c5150968b48cdbc21ee7d5e65
Fahim Tazwoar Sourav <tazwoarlives@gmail.com>
2025-11-02 21:34:47 +0600

    recaping object

--- M2V1.js
+++ M2V1.js
@@ -1,1 +1,8 @@
-// Map -----------> Introduction
+// Map (data structure) -----------> Introduction
+
+const object = {
+    nextLevel : {courseId : "Level-2"} ,
+    "Programming Hero" : {courseId : "Level-1"} ,
+} ;
+
+console.log(object.nextLevel);
