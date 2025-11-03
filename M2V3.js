e67eb9ac0758e38919c0bf0772537fed3428a01e
Fahim Tazwoar Sourav <tazwoarlives@gmail.com>
2025-11-03 23:58:27 +0600

    array flat

--- M2V3.js
+++ M2V3.js
@@ -5,8 +5,8 @@ const nums = [55 , 2 , 10 , 1100 , 90, 5]
 
 const sortedNums = nums.sort() ; 
 
-console.log(sortedNums);
-console.log(nums);
+// console.log(sortedNums);
+// console.log(nums);
 
 //
 
@@ -16,7 +16,17 @@ const fruits2 = ["apples", "Banana", "Grapes", "peer", "avocado"]
 const basket = fruits.sort() ;
 const basket2 = fruits2.sort((a, b) => b.localeCompare(a)) ;
 
-console.log(basket);
-console.log(fruits);
-console.log(fruits2);
-console.log(basket2);
+// localeCompare used to ignore
+
+// console.log(basket);
+// console.log(fruits);
+// console.log(fruits2);
+// console.log(basket2);
+
+const arr1 = [1,2,3,[4,[5,[6,7,8,9]]]] ; 
+
+const arr2 = arr1.flat();
+const arr3 = arr2.flat(2)
+
+console.log(arr2);
+console.log( arr3);
