10647e4b0e932cd99b6d9adbcd5fc8a4eb33ac6f
Fahim Tazwoar Sourav <tazwoarlives@gmail.com>
2025-11-03 23:29:03 +0600

    new about sorting

--- M2V3.js
+++ M2V3.js
@@ -1,1 +1,18 @@
 // 2-3 Sorting and flattening array explained
+const a = "apple"
+
+const nums = [55 , 2 , 10 , 1100 , 90, 5]
+
+const sortedNums = nums.sort() ; 
+
+console.log(sortedNums);
+console.log(nums);
+
+//
+
+const fruits = ["apples", "Banana", "Grapes", "peer", "avocado"]
+
+const basket = fruits.sort() ;
+
+console.log(basket);
+console.log(fruits);
