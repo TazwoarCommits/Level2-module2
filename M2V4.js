25d878d79d7e93ce4c224b2dcc863c34a7eddf9f
Fahim Tazwoar Sourav <tazwoarlives@gmail.com>
2025-11-04 19:30:38 +0600

    learned (.some() method)

--- M2V4.js
+++ M2V4.js
@@ -1,2 +1,55 @@
 // ======> Array cross matching and Array.from()
 
+
+
+
+// --------> .some
+// const numbers = [1,2,3,4,5] ; 
+const numbers = [1,3,7,5] ; 
+
+const hasEvenNum = numbers.some(num => num % 2 === 0 ) ; 
+
+// console.log(hasEvenNum);
+
+const currentUserRole = ["user","editor"] ;
+
+const featureAccessRoles = ["admin","manager"]
+
+
+const canAccess = currentUserRole.some( role => featureAccessRoles.includes(role)) ;
+
+console.log(canAccess);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
